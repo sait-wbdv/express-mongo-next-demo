bdea2d39d0dbcc709df5b7a7685a5fffc349e412
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-24 23:46:11 -0700

    simple dynamic route example

--- server/index.js
+++ server/index.js
@@ -6,9 +6,21 @@ const PORT = process.env.PORT || 3000;
 const Comment = require("./model/Comment");
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("/comments", async (req, res) => {
   const result = await Comment.find().limit(25);
-  res.send({ Comments: result });
+  res.send({ comments: result });
+});
+
+app.get("/comments/:id", async (req, res) => {
+  try {
+    const result = await Comment.findOne({ _id: req.params.id });
+    if (!result) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+    res.json({ comment: result });
+  } catch (error) {
+    console.error("Error", error);
+  }
 });
 
 // connect to db
